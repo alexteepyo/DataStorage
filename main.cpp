2f90700ed63699930b3b11caddef3a30610633a3
alexteepyo <112500777+alexteepyo@users.noreply.github.com>
2024-03-29 10:15:26 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    string easyQuestions[20] = {"Can a kangaroo jump higher than the Burj Khalifa?", "Are british people real?", "Do you have a deadly disease or do you just look like that?", "What is Mr.Rogan's full name?", "Who is the best programmer?", "Why did the tech crew scream with fear?", "Is this fun?", "Why did the phone weigh more than your mother?", "How old is Queen Elizabeth", "Why did the chicken cross the road?", "dis", "anti", "im", "mis", "ex", "en", "inter", "pan", "astro", "post"};
+    int attempts = 0;
+
+    cout << "Welcome to Alex's wonderful trivia! Do you want to play? Just kidding! You have to :)" << endl;
+
+    
+}
